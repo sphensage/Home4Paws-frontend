314e68a34a89aabc03b4214c8355a9c5927d32e1
SphenSage <jeremi.parel@gmail.com>
2026-01-25 15:36:39 +0800

    Implemented react router DOM for webpage routing.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import LoginPage from "./webpages/LoginPage.tsx";
+import HomePage from "./webpages/HomePage.tsx";
+import SignUpPage from "./webpages/SignUpPage.tsx";
+import ErrorPage from "./webpages/ErrorPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
