9bde0a4b61690ca3161571e6a1e08658ed894eec
SphenSage <jeremi.parel@gmail.com>
2026-02-03 20:53:00 +0800

    Made dummy components (inbox & post)

--- src/components/dummies/DummyPostItem.tsx
+++ src/components/dummies/DummyPostItem.tsx
@@ -12,7 +12,6 @@ const DummyPostItem = () => {
         height: "4.5rem",
       }}
     >
-      {/* Left side: Matches the col-6 and ps-4 of the header */}
       <div className="col-6 d-flex flex-row justify-content-start align-items-center ps-4">
         <div
           id="profileIconBox"
@@ -22,17 +21,15 @@ const DummyPostItem = () => {
           <img src="/src/assets/user_icon.svg" alt="user icon" />
         </div>
         <div id="postDescription" className="ms-3 fw-bold text-truncate">
-          giving out free doggos =w=
+          giving out free doggos =w= this text is very very long longer than any
+          title
         </div>
       </div>
 
-      {/* Right side: Matches the col-6 and justify-content-around of the header */}
       <div className="d-flex flex-row col-6 justify-content-around">
-        {/* Matches col-3 and text-center of the header */}
         <div id="datePosted" className="col-3 text-center mb-0">
           9 hours ago
         </div>
-        {/* Matches col-3 and text-center of the header */}
         <div id="likes" className="col-3 text-center mb-0">
           8
         </div>
